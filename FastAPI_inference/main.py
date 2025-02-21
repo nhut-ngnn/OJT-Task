44a4095b520c5baf9b8d255316e6ee17576eecf2
nhut-ngnn <nhutnmse184534@fpt.edu.v>
2025-02-21 08:06:25 +0700

    FastAPI: install requirements

--- FastAPI_inference/main.py
+++ FastAPI_inference/main.py
@@ -8,36 +8,29 @@ import io
 
 app = FastAPI()
 
-# Define image transformation
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
-# Triton Client
 client = httpclient.InferenceServerClient(url="localhost:8000")
 
 @app.post("/infer/")
 async def infer(image: UploadFile = File(...)):
     try:
-        # Read image
         image_bytes = await image.read()
         img = Image.open(io.BytesIO(image_bytes)).convert("RGB")
         
-        # Transform image
         image_tensor = transform(img)
         transformed_img = image_tensor.numpy().astype(np.float32)
         transformed_img = np.expand_dims(transformed_img, axis=0)  # Add batch dimension
         
-        # Prepare Triton input
         inputs = httpclient.InferInput("data_0", transformed_img.shape, datatype="FP32")
         inputs.set_data_from_numpy(transformed_img, binary_data=True)
         
-        # Define output
         outputs = httpclient.InferRequestedOutput("fc6_1", binary_data=True)
         
-        # Perform inference
         results = client.infer(model_name="densenet_onnx", inputs=[inputs], outputs=[outputs])
         inference_output = results.as_numpy('fc6_1')
         
